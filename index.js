8bcde994f8fe306ab6995ac440c012e0d497de2e
Julio Londono <juliolondonogarcia@gmail.com>
2019-03-13 23:56:53 +0000

    version estable en la parte de vericacion de la entrda de datos

--- index.js
+++ index.js
@@ -1,59 +1,73 @@
 // JavaScript File
 
- let dealFunction = () => {
-  
-  // Disable button after click
-  
-    // document.getElementById("newCards").onclick = function() {
-    // //disable
-    // this.disabled = true;
-    // };
-  
-  // Read value from the input
-    let inputtxt = document.querySelector("#numberOfCards").value;
-      // console.log("input=" + inputtxt);
+
+/////// Esta funcion verifica que el valor entrado es un numero y esta dentro del rango y lo almacena en el HTML///////////////////
+
+document.getElementById("newCards").addEventListener("click", handleInput);
+
+function handleInput() {
+  let inputValue = document.querySelector("#numberOfCards").value;
+  // console.log("inputValue=" + inputValue);
   //verify if the input is valid
-    let textInput = isNaN(inputtxt);
-      // console.log("isNaN" + textInput);
+    let textInput = isNaN(inputValue);
+      // console.log("is Not a Number= " + textInput);
       
     let rango = false;
-    if(inputtxt>1 && inputtxt<14) {
+    if(inputValue>1 && inputValue<14) {
       rango = true;
     }
-    
     // console.log("rango=" + rango);
-    let validEntry = false;
+    
+      let validEntry = false;
       
-    if (rango===false || textInput===true) {
-      alert("Please enter a number between 2 and 13");
-      window.location.reload();  
-      return;
-    } else {
+      if (rango===false || textInput===true) {
+        alert("Please enter a number between 2 and 13");
+        window.location.reload();  
+        validEntry = false;
+        return;
+      } else {
       
-      validEntry =true;
+        validEntry =true;
       // console.log("valid entry:" + validEntry );
-    }
+      }
     
-    // if ( validEntry === true) {
-    //   window.location.reload();  
-    // }
-    
-    //create the divs based on the amount of cards entered
-    for (var i=0; i<inputtxt-1; i++) {
-      
-      const sourceElement = document.querySelector('.card');
-      const destination = document.querySelector('.flex-container');
+      if ( validEntry === true) {
+      // console.log("numero a pasar por la funcion=" + inputValue);
+      document.getElementById("result").innerHTML = inputValue;
+      return inputValue;
+      }
+}
 
-      const copy = sourceElement.cloneNode(true);
-      destination.appendChild(copy);
-    }
+
+//////// Esta funcion lee la cantidad de cartas y crea los divs en el dom con valores aleatorios///////////////////////////////////////
+document.getElementById("newCards").addEventListener("click", dealFunction);
+
+function dealFunction() {
+  
+//   //borra las cartas que hayan sido crerada en un click anterior
+//   while (document.querySelector('.flex-container').hasChildNodes()) {
+//     document.querySelector('.flex-container').removeChild(document.querySelector('.flex-container').lastChild);
+// }
+  
+  let inputtxt = document.querySelector("#numberOfCards").value;
+  console.log("captura de result=" + inputtxt);
+  
+//create the divs based on the amount of cards entered
+  for (var i=0; i<inputtxt-1; i++) {
+      
+    const sourceElement = document.querySelector('.card');
+    const destination = document.querySelector('.flex-container');
+
+    const copy = sourceElement.cloneNode(true);
+    destination.appendChild(copy);
+  }
     
-  // create the arrays that will be used by the sorting algoritm
+// create the arrays that will be used by the sorting algoritm
   
   let cardsArray = [];
   let sutesArray = [];
   
-  // generate random values
+// generate random values
   let divs = document.querySelectorAll(".card");
     // console.log(divs);
 
@@ -68,7 +82,7 @@
     // console.log(valueRand);
     cardsArray.push(valueRand);
     
-  // assign the face of the card based on a random value
+// assign the face of the card based on a random value
     switch (sutesRand) {
       case 1:
         divs[i].querySelector(".fondo").src = "/resources/images/diamondSmall.jpg" ;
@@ -100,21 +114,27 @@
       divs[i].querySelector(".centered").innerHTML = valueRand;
     }
   }
+  document.getElementById("faces").innerHTML = sutesArray;
+  document.getElementById("numbers").innerHTML = cardsArray;
   // console.log("sutes Array= " + sutesArray);
   // console.log("Cards Array =" + cardsArray);
-  let result = [sutesArray, cardsArray];
-  return result;
-};
+  return [sutesArray, cardsArray];
+}
+
+let sutesArray = document.getElementById("faces").innerHTML.value;
+console.log("sutesArray=" + sutesArray);
+
+let cardsArray = document.getElementById("numbers").innerHTML.value;
+console.log("CardsArray=" + cardsArray);
 
 
-let cosas = dealFunction;
- console.log(cosas[0]);
-
-//let sutesArray = dealArrays[0];
-//let cardsArray = dealArrays[1];
-// console.log("sutes Array= " + sutesArray);
-// console.log("Cards Array =" + cardsArray);
 
 
+// let dealArrays = dealFunction();
+// let faces =dealArrays[0]
+// let valores = dealArrays[1];
+// console.log(dealArrays);
+// console.log(faces);
+// console.log(valores);
 
 
